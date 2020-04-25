e09a5d004bf6c9d7824065b3cd3af1f9a8ce1f3f
Damian <damiandanielvigo@gmail.com>
2020-04-25 00:34:09 -0300

    update-bem

--- js/main.js
+++ js/main.js
@@ -1,221 +1,216 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Efecto h1 y texto del header
   if ($(window).width() > 800) {
-    $("header .texto").css({
+    $('header .contenedor-texto__texto').css({
       opacity: 0,
-      marginTop: 0
+      marginTop: 0,
     });
 
-    $("header .contenedor-texto .texto").animate(
+    $('header .contenedor-texto__texto').animate(
       {
         opacity: 1,
-        marginTop: "-52px"
+        marginTop: '-52px',
       },
       1500
     );
   }
   // Scroll Elementos Menu
-  var bienvenido = $("#bienvenido").offset().top,
-    sobreMi = $("#acerca-de").offset().top,
-    proyectos = $("#proyectos").offset().top,
-    contacto = $("#contacto").offset().top;
+  var bienvenido = $('#bienvenido').offset().top,
+    sobreMi = $('#acerca-de').offset().top,
+    proyectos = $('#proyectos').offset().top,
+    contacto = $('#contacto').offset().top;
 
-  $("#btn-bienvenido").on("click", function(e) {
+  $('#btn-bienvenido').on('click', function (e) {
     e.preventDefault();
-    $("html, body").animate(
+    $('html, body').animate(
       {
-        scrollTop: bienvenido
+        scrollTop: bienvenido,
       },
       500
     );
   });
 
-  $("#btn-sobre-mi").on("click", function(e) {
+  $('#btn-sobre-mi').on('click', function (e) {
     e.preventDefault();
-    $("html, body").animate(
+    $('html, body').animate(
       {
-        scrollTop: 300
+        scrollTop: 300,
       },
       500
     );
   });
 
-  $("#btn-proyectos").on("click", function(e) {
+  $('#btn-proyectos').on('click', function (e) {
     e.preventDefault();
-    $("html, body").animate(
+    $('html, body').animate(
       {
-        scrollTop: proyectos
+        scrollTop: proyectos,
       },
       500
     );
   });
 
-  $("#btn-contacto").on("click", function(e) {
+  $('#btn-contacto').on('click', function (e) {
     e.preventDefault();
-    $("html, body").animate(
+    $('html, body').animate(
       {
-        scrollTop: contacto
+        scrollTop: contacto,
       },
       500
     );
   });
   // Parallax
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     var windowWidth = $(window).width();
     if (windowWidth > 800) {
       var scroll = $(window).scrollTop();
-      $("header .contenedor-texto").css({
-        transform: "translate(0px," + scroll / 2 + "%)"
+      $('header .contenedor-texto').css({
+        transform: 'translate(0px,' + scroll / 2 + '%)',
       });
     }
   });
   // Enlaces activos
-  $("#menu a").on("click", function() {
-    $("a.activo").removeClass("activo");
-    $(this).addClass("activo");
+  $('#menu a').on('click', function () {
+    $('a.activo').removeClass('activo');
+    $(this).addClass('activo');
   });
 
-  $(function() {
-    var menu = document.getElementById("menu");
+  $(function () {
+    var menu = document.getElementById('menu');
     var headroom = new Headroom(menu);
     headroom.init();
 
     //Menu Responsive
     //Ancho de la página
     var ancho = $(window).width(),
-      enlaces = $("#enlaces"),
-      btnMenu = $("#btn-menu"),
-      icono = $("#btn-menu .icono");
+      enlaces = $('#enlaces'),
+      btnMenu = $('#btn-menu'),
+      icono = $('#btn-menu .icono');
 
     if (ancho < 768) {
       enlaces.hide();
-      icono.addClass("fa-bars");
+      icono.addClass('fa-bars');
     }
 
-    btnMenu.on("click", function(e) {
+    btnMenu.on('click', function (e) {
       enlaces.slideToggle();
-      icono.toggleClass("fa-bars");
-      icono.toggleClass("fa-times");
+      icono.toggleClass('fa-bars');
+      icono.toggleClass('fa-times');
     });
 
-    $(window).on("resize", function() {
+    $(window).on('resize', function () {
       if ($(this).width() > 768) {
         enlaces.show();
-        icono.addClass("fa-times");
-        icono.removeClass("fa-bars");
+        icono.addClass('fa-times');
+        icono.removeClass('fa-bars');
       } else {
         enlaces.hide();
-        icono.addClass("fa-bars");
-        icono.removeClass("fa-times");
+        icono.addClass('fa-bars');
+        icono.removeClass('fa-times');
       }
     });
   });
 });
 
 // Slider
-$(window).on("load", function() {
-  $("#slider").nivoSlider();
+$(window).on('load', function () {
+  $('#slider').nivoSlider();
 });
 
-(function() {
+(function () {
   // Google Analytics
   window.dataLayer = window.dataLayer || [];
   function gtag() {
     dataLayer.push(arguments);
   }
-  gtag("js", new Date());
+  gtag('js', new Date());
 
-  gtag("config", "UA-145056950-1");
+  gtag('config', 'UA-145056950-1');
 
   // Materialize
-  document.addEventListener("DOMContentLoaded", () => {
-    const imgLightBox = document.querySelectorAll(".materialboxed");
+  document.addEventListener('DOMContentLoaded', () => {
+    const imgLightBox = document.querySelectorAll('.materialboxed');
     M.Materialbox.init(imgLightBox, {
       inDuration: 500,
-      outDuration: 500
+      outDuration: 500,
     });
   });
 
   // Imagenes Cargadas
-  window.addEventListener("load", () => {
-    const proyectos = document.getElementById("contenedor-proyectos");
-    proyectos.classList.add("proyectos-cargados");
+  window.addEventListener('load', () => {
+    const proyectos = document.getElementById('contenedor-proyectos');
+    proyectos.classList.add('proyectos-cargados');
   });
   // Ruta de la imagen y descripción para el overlay
-  const overlay = document.getElementById("overlay");
-  document.querySelectorAll(".proyectos .thumb img").forEach(elemento => {
+  const overlay = document.getElementById('overlay');
+  document.querySelectorAll('.proyectos__img').forEach((elemento) => {
     // Muestro el Overlay para cada imagen
-    elemento.addEventListener("click", () => {
-      const ruta = elemento.getAttribute("src");
+    elemento.addEventListener('click', () => {
+      const ruta = elemento.getAttribute('src');
       const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
 
-      overlay.classList.add("activo");
-      document.querySelector("#overlay img").src = ruta;
-      document.querySelector("#overlay .descripcion").innerHTML = descripcion;
+      overlay.classList.add('activo');
+      document.querySelector('#overlay img').src = ruta;
+      document.querySelector('#overlay .descripcion').innerHTML = descripcion;
     });
   });
 
   // botón cerrar
-  document
-    .querySelector("#btn-cerrar-lightbox")
-    .addEventListener("click", () => {
-      overlay.classList.remove("activo");
-    });
+  document.querySelector('#btn-cerrar-lightbox').addEventListener('click', () => {
+    overlay.classList.remove('activo');
+  });
 
-  overlay.addEventListener("click", evento => {
-    evento.target.id === "overlay" ? overlay.classList.remove("activo") : "";
+  overlay.addEventListener('click', (evento) => {
+    evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
   });
 
   // Validacion formulario
-  var formulario = document.getElementById("formulario"),
+  var formulario = document.getElementById('formulario'),
     nombre = formulario.nombre,
     correo = formulario.correo,
     mensaje = formulario.mensaje,
-    error = document.getElementById("error");
+    error = document.getElementById('error');
 
   function validarNombre(e) {
-    if (nombre.value == "" || nombre.value == null) {
-      error.style.display = "block";
-      error.innerHTML += "<li>Por favor completa tu nombre</li>";
+    if (nombre.value == '' || nombre.value == null) {
+      error.style.display = 'block';
+      error.innerHTML += '<li>Por favor completa tu nombre</li>';
 
       e.preventDefault();
     } else {
-      error.style.display = "none";
+      error.style.display = 'none';
     }
   }
 
   function validarCorreo(e) {
-    if (correo.value == "" || correo.value == null) {
-      error.style.display = "block";
-      error.innerHTML += "<li>Por favor completa tu mail</li>";
+    if (correo.value == '' || correo.value == null) {
+      error.style.display = 'block';
+      error.innerHTML += '<li>Por favor completa tu mail</li>';
 
       e.preventDefault();
     } else {
-      error.style.display = "none";
+      error.style.display = 'none';
     }
   }
 
   function validarMensaje() {
-    if (mensaje.value == "" || mensaje.value == null) {
-      error.style.display = "block";
-      error.innerHTML += "<li>Por favor dejame tu mensaje</li>";
+    if (mensaje.value == '' || mensaje.value == null) {
+      error.style.display = 'block';
+      error.innerHTML += '<li>Por favor dejame tu mensaje</li>';
 
       e.preventDefault();
     } else {
-      error.style.display = "none";
+      error.style.display = 'none';
     }
   }
 
   function validarFormulario(e) {
-    error.innerHTML = "";
+    error.innerHTML = '';
 
     validarNombre(e);
     validarCorreo(e);
     validarMensaje(e);
   }
 
-  formulario.addEventListener("submit", validarFormulario);
-
+  formulario.addEventListener('submit', validarFormulario);
 })();
-
-
