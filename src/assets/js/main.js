8a61ee42224991910f988b2d51eb5d1459508a2e
Damian <damiandanielvigo@gmail.com>
2020-04-26 14:10:59 -0300

    update-local-storage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -170,7 +170,23 @@ $(window).on('load', function () {
   btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
+
+    // Guardo el modo en localstorage
+    if (document.body.classList.contains('dark')) {
+      localStorage.setItem('dark-mode', 'true');
+    } else {
+      localStorage.setItem('dark-mode', 'false');
+    }
   });
+
+    // El modo actual
+  if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+  } else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+  }
 
   // Validacion formulario
   /*   var formulario = document.getElementById('formulario'),
